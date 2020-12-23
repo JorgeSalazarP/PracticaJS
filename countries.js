7ba63fc138ea6fda28f872d2b7d809e1ac66fcc3
Jorge Salazar <j-salazarp@hotmail.com>
2020-12-23 18:03:01 +0100

    Creamos los arrays con los países participantes en la competeción

---
+++ countries.js
@@ -0,0 +1,5 @@
+const europe = ['España','Alemania','Italia','Francia','Portugal','Suecia','Irlanda', 'Inglaterra','Rusia','Escocia','Croacia','Belgica'];
+const america = ['Argentina','Brasil','Uruguay','Paraguay','Colombia','USA','Mexico','Costa Rica','Chile'];
+const africa = ['Camerun','Marruecos','Sudafrica','Senegal','Costa de Marfil'];
+const oceania = ['Australia','Nueva Zelanda'];
+const asia = ['Corea del Sur', 'Iran','China', 'India'];
